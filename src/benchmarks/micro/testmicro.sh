516ae178128530d1707384b137cc1a1eb16cc1bd
administrator <jeducb@gmail.com>
2023-06-28 21:32:25 +0000

    Update testing code

--- src/benchmarks/micro/testmicro.sh
+++ src/benchmarks/micro/testmicro.sh
@@ -8,8 +8,7 @@
 sudo LD_PRELOAD=/lib/x86_64-linux-gnu/libssl.so.1.1:/lib/x86_64-linux-gnu/libcrypto.so.1.1:/usr/lib/x86_64-linux-gnu/engines-1.1/qatengine.so \
      DOTNET_PROCESSOR_COUNT=$1 OPENSSL_ASYNC_ENABLED=$2 QAT_ENGINE=QAT_$3 \
      dotnet run -c Release -f net8.0 --statisticalTest $4 --filter System.Net.Security.Tests.SslStreamTests.$5 \
-     --coreRun "/home/administrator/git/innersource/dotnet.runtime.main/artifacts/bin/testhost/net8.0-linux-Release-x64/shared/Microsoft.NETCore.App/8.0.0/corerun" \
-     "/home/administrator/git/innersource/dotnet.runtime.poc/artifacts/bin/testhost/net8.0-linux-Release-x64/shared/Microsoft.NETCore.App/8.0.0/corerun" --hide Job Toolchain --apples --iterationCount $6
+     --coreRun "/home/administrator/git/innersource/dotnet.runtime.poc/artifacts/bin/testhost/net8.0-linux-Release-x64/shared/Microsoft.NETCore.App/8.0.0/corerun" --hide Job Toolchain
 
 # sudo LD_PRELOAD=/usr/lib/x86_64-linux-gnu/engines-1.1/libplock.so OPENSSL_ASYNC_ENABLED=$1 QAT_ENGINE=QAT_$2 \
 #      dotnet run -c Release -f net8.0 --statisticalTest $3 --filter System.Net.Security.Tests.SslStreamTests.$4 \
