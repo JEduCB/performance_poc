e41cf23b7a55e7cc5675bef7172971d14136ede7
administrator <jeducb@gmail.com>
2023-06-28 21:32:25 +0000

    Update testing code

--- src/benchmarks/micro/testmicro.sh
+++ src/benchmarks/micro/testmicro.sh
@@ -5,10 +5,10 @@
 #DOTNET_SYSTEM_NET_SECURITY_DISABLETLSRESUME=1
 #LD_PRELOAD=/usr/lib/x86_64-linux-gnu/engines-1.1/libplock.so
 
-sudo DOTNET_EnableAVX512F=1 LD_PRELOAD=/usr/lib/x86_64-linux-gnu/engines-1.1/libplock.so DOTNET_PROCESSOR_COUNT=$1 OPENSSL_ASYNC_ENABLED=$2 QAT_ENGINE=QAT_$3 ENGINE_METHOD=$4 \
-     dotnet run -c Release -f net8.0 --statisticalTest $5 --filter System.Net.Security.Tests.SslStreamTests.$6 \
-     --coreRun "/home/administrator/git/innersource/dotnet.runtime.poc/artifacts/bin/testhost/net8.0-linux-Release-x64/shared/Microsoft.NETCore.App/8.0.0/corerun" \
-     --hide Job Toolchain --iterationCount $7
+sudo LD_PRELOAD=/lib/x86_64-linux-gnu/libssl.so.1.1:/lib/x86_64-linux-gnu/libcrypto.so.1.1:/usr/lib/x86_64-linux-gnu/engines-1.1/qatengine.so \
+     DOTNET_PROCESSOR_COUNT=$1 OPENSSL_ASYNC_ENABLED=$2 QAT_ENGINE=QAT_$3 \
+     dotnet run -c Release -f net8.0 --statisticalTest $4 --filter System.Net.Security.Tests.SslStreamTests.$5 \
+     --coreRun "/home/administrator/git/innersource/dotnet.runtime.poc/artifacts/bin/testhost/net8.0-linux-Release-x64/shared/Microsoft.NETCore.App/8.0.0/corerun" --hide Job Toolchain
 
 # sudo LD_PRELOAD=/usr/lib/x86_64-linux-gnu/engines-1.1/libplock.so OPENSSL_ASYNC_ENABLED=$1 QAT_ENGINE=QAT_$2 \
 #      dotnet run -c Release -f net8.0 --statisticalTest $3 --filter System.Net.Security.Tests.SslStreamTests.$4 \
